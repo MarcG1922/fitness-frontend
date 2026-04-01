ac5a78c637621ed05fd70452e30cae5edc334b8d
Marc Granero <magrba@floridauniversitaria.es>
2026-04-01 11:29:00 +0200

    Conectar frontend con backend en Railway

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://fitness-backend-production-6380.up.railway.app/api"
 });
 
 export default api;
